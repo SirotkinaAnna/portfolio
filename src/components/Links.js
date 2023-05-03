bf67ce7fce74393106e366bd1e3b164b706ad3ab
SirotkinaAnna <saurann88@gmail.com>
2023-05-03 16:07:49 -0400

    Update Links.js

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,8 +1,8 @@
 function Links() {
     return <div className=" text-uppercase">
-        <a href="/" className="me-3 text-decoration-none">Home</a>
-        <a href="/portfolio" className="mx-3 text-decoration-none">Portfolio</a>
-        <a href="#contacts" className="ms-3 text-decoration-none">Contact me</a>
+        <a href="/" className="me-1 me-md-3 text-decoration-none">Home</a>
+        <a href="/portfolio" className="mx-1 mx-md-3 text-decoration-none">Portfolio</a>
+        <a href="#contacts" className="ms-1 ms-md-3 text-decoration-none">Contact me</a>
     </div>
 }
 export default Links;
